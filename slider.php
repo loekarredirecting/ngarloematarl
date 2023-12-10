f6d21c68f66368bdde53d189be8d977cae5df2bb
htetrkar <95409875+loekarredirecting@users.noreply.github.com>
2023-12-10 23:15:04 +0630

    Create slider.php

---
+++ slider.php
@@ -0,0 +1,3 @@
+[{ "title": "New Live", "link": "https://play.google.com/store/apps/details?id=com.pro.burmese_tv", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/avietnam.jpg" },
+{ "title": "Advice", "link": "https://m.me/109810678671415", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/advice.jpg" }, 
+{ "title": "Advertising", "link": "https://play.google.com/store/apps/details?id=com.pro.burmese_tv", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/adslocal.jpg" }]
