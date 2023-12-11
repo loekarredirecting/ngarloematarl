88c4165980b30dd6e73fd32799702639f30d4326
htetrkar <95409875+loekarredirecting@users.noreply.github.com>
2023-12-11 23:41:44 +0630

    Update mainlive.php

--- mainlive.php
+++ mainlive.php
@@ -1,82 +1,41 @@
 [
 {
-"league": " King's Cup ",
-"himg": "https://images.fotmob.com/image_resources/logo/teamlogo/2529_small.png",
-"hteam": "Al-Taawoun",
-"time": " live ",
-"date": "9:15 PM",
-"aimg": "https://images.fotmob.com/image_resources/logo/teamlogo/205686_small.png",
-"ateam": "Al Hilal",
-"link": [
-{
-"type": "English 1 FHD",
-"livelink": "https://headlinessportsfoot.com/chunklist/hlsch2.m3u8",
-"trylink": ""
-},
-{
-"type": "English 2 HD",
-"livelink": "https://usa.raino.site/hls/stream2.m3u8",
-"trylink": "https://embed4u.xyz/"
-},
-{
-"type": "Vietnam HD",
-"livelink": "https://live.vbfast.xyz/14494b9c057c0ba6fac7d1c78eb3db7f/1702348516173/rchtwtx/rchtwtx@720p.m3u8",
-"trylink": "https://xlz.plvb.xyz/"
-},
-{
-"type": "Vietnam FHD",
-"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel1.m3u8?auth_key=3000001702305354-0-0-48ffdb46e2c290b9c4729496520b93bc",
-"trylink": ""
-},
-{
-"type": "China HD",
-"livelink": "https://a0504dae9c72cdb02b00ddedf6df4973.livehwc3.cn/pull.juqihuo.com/live/stream-492306_lhd.m3u8?user_session_id=ebefcd88fda2d6e6f5daae25d568f5d8&sub_m3u8=true&edge_slice=true&auth_key=1702308844-0-0-72738ae0bbeb91aabbc7e2b8e95d119c",
-"trylink": ""
-},
-{
-"type": "China SD",
-"livelink": "https://a0504dae9c72cdb02b00ddedf6df4973.livehwc3.cn/pull.juqihuo.com/live/stream-492306_lsd.m3u8?user_session_id=ebefcd88fda2d6e6f5daae25d568f5d8&sub_m3u8=true&edge_slice=true&auth_key=1702308844-0-0-72738ae0bbeb91aabbc7e2b8e95d119c",
-"trylink": ""
-},
-{
-"type": "Soco HD",
-"livelink": "https://pull01.rrwes.live/live/stream-9912041_lhd.m3u8?txSecret=39c67ccadfb0d84051abaab99d2bbdfd&txTime=6577722d",
-"trylink": ""
-},
-{
-"type": "Soco SD",
-"livelink": "https://pull01.rrwes.live/live/stream-9912041_lsd.m3u8?txSecret=39c67ccadfb0d84051abaab99d2bbdfd&txTime=6577722d",
-"trylink": ""
-}
-]
-},
-{
 "league": " Serie A ",
 "himg": "https://images.fotmob.com/image_resources/logo/teamlogo/8534_small.png",
 "hteam": "Lecce",
-"time": " 12:00 AM ",
-"date": "2023-12-12",
+"time": " live ",
+"date": "12:00 AM",
 "aimg": "https://images.fotmob.com/image_resources/logo/teamlogo/9888_small.png",
 "ateam": "Empoli",
 "link": [
 {
+"type": "English 1 FHD",
+"livelink": "https://plpl12.teujyt.space/playlist/17114/red111.snflsb.space/caxi.m3u8",
+"trylink": "https://1stream.eu/"
+},
+{
+"type": "English 2 HD",
+"livelink": "https://killeredw.site/live/abr_ch1/live/ch1/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTIvMTEvMjAyMyA1OjExOjIzIFBNJmhhc2hfdmFsdWU9TG15Rlc4d2VYdHVaS1VJdld1djU0UT09JnZhbGlkbWludXRlcz03MjAmaWQ9MjAyLjE2NS44Ni42NCZzdHJtX2xlbj01",
+"trylink": "https://streams.score808.football/"
+},
+{
 "type": "Vietnam HD",
-"livelink": "0",
+"livelink": "https://live.vbfast.xyz/00e355d838c29f0507279acaf9270468/1702357559952/r8ztat6/r8ztat6@720p.m3u8",
 "trylink": "https://xlz.plvb.xyz/"
 },
 {
 "type": "Vietnam FHD",
-"livelink": "0",
+"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel6.m3u8?auth_key=3000001702314373-0-0-91f7c2ad0e875a24763c34c9de4c1b6f",
 "trylink": ""
 },
 {
 "type": "China HD",
-"livelink": "0",
+"livelink": "https://a0504dae9c72cdb02b00ddedf6df4973.livehwc3.cn/pull.juqihuo.com/live/stream-710099_lhd.m3u8?user_session_id=8ad72770c2f47accb82d6d01377749fa&auth_key=1702318024-0-0-756a9a7296375d08bf50a4c75f6ab3ba&edge_slice=true&sub_m3u8=true",
 "trylink": ""
 },
 {
 "type": "China SD",
-"livelink": "0",
+"livelink": "https://a0504dae9c72cdb02b00ddedf6df4973.livehwc3.cn/pull.juqihuo.com/live/stream-710099_lsd.m3u8?user_session_id=8ad72770c2f47accb82d6d01377749fa&auth_key=1702318024-0-0-756a9a7296375d08bf50a4c75f6ab3ba&edge_slice=true&sub_m3u8=true",
 "trylink": ""
 },
 {
@@ -95,19 +54,19 @@
 "league": " King's Cup ",
 "himg": "https://images.fotmob.com/image_resources/logo/teamlogo/101916_small.png",
 "hteam": "Al Nassr FC",
-"time": " 12:30 AM ",
-"date": "2023-12-12",
+"time": " live ",
+"date": "12:30 AM",
 "aimg": "https://images.fotmob.com/image_resources/logo/teamlogo/101918_small.png",
 "ateam": "Al Shabab",
 "link": [
 {
 "type": "Vietnam HD",
-"livelink": "0",
+"livelink": "https://live.vbfast.xyz/eb7c98d376edfa0f40a6cca28674384b/1702357616679/lhsazur/index.m3u8",
 "trylink": "https://xlz.plvb.xyz/"
 },
 {
 "type": "Vietnam FHD",
-"livelink": "0",
+"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel2.m3u8?auth_key=3000001702314430-0-0-079b860a01bc48b2c76b8429687c3b8b",
 "trylink": ""
 },
 {
