bdfa73519dda4c86b7baa9923e1f977b206dc2df
htetrkar <95409875+loekarredirecting@users.noreply.github.com>
2023-12-14 00:02:31 +0630

    Update mainlive.php

--- mainlive.php
+++ mainlive.php
@@ -3,29 +3,29 @@
 "league": " Champions League ",
 "himg": "https://images.fotmob.com/image_resources/logo/teamlogo/8687_small.png",
 "hteam": "Man City",
-"time": " 12:15 AM ",
-"date": "2023-12-14",
+"time": " live ",
+"date": "12:15 AM",
 "aimg": "https://images.fotmob.com/image_resources/logo/teamlogo/8456_small.png",
 "ateam": "FK Crvena Zvezda",
 "link": [
 {
 "type": "Vietnam HD",
-"livelink": "0",
+"livelink": "https://live.vbfast.xyz/67723ecf81c2870b17580e3c70581101/1702531630815/rchtwtx/rchtwtx@720p.m3u8",
 "trylink": "https://xlz.plvb.xyz/"
 },
 {
 "type": "Vietnam FHD",
-"livelink": "0",
+"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel5.m3u8?auth_key=3000001702488452-0-0-9cb93d5dacd0463aa742e229a18c720a",
 "trylink": ""
 },
 {
 "type": "China HD",
-"livelink": "0",
+"livelink": "https://a0504dae9c72cdb02b00ddedf6df4973.livehwc3.cn/pull.juqihuo.com/live/stream-746673_lhd.m3u8?edge_slice=true&sub_m3u8=true&user_session_id=31c65abe196926af1d6c081e8ee918c6&auth_key=1702492205-0-0-a028b597f39d5cac876480c29fadb4b4",
 "trylink": ""
 },
 {
 "type": "China SD",
-"livelink": "0",
+"livelink": "https://a0504dae9c72cdb02b00ddedf6df4973.livehwc3.cn/pull.juqihuo.com/live/stream-746673_lsd.m3u8?edge_slice=true&sub_m3u8=true&user_session_id=31c65abe196926af1d6c081e8ee918c6&auth_key=1702492205-0-0-a028b597f39d5cac876480c29fadb4b4",
 "trylink": ""
 },
 {
@@ -44,29 +44,29 @@
 "league": " Champions League ",
 "himg": "https://images.fotmob.com/image_resources/logo/teamlogo/178475_small.png",
 "hteam": "Young Boys",
-"time": " 12:15 AM ",
-"date": "2023-12-14",
+"time": " live ",
+"date": "12:15 AM",
 "aimg": "https://images.fotmob.com/image_resources/logo/teamlogo/10192_small.png",
 "ateam": "RB Leipzig",
 "link": [
 {
 "type": "Vietnam HD",
-"livelink": "0",
+"livelink": "https://live.vbfast.xyz/270c2f1dcab1991ef22cf47d5c070f9c/1702531716888/r8ztat6/r8ztat6@720p.m3u8",
 "trylink": "https://xlz.plvb.xyz/"
 },
 {
 "type": "Vietnam FHD",
-"livelink": "0",
+"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel15.m3u8?auth_key=3000001702488551-0-0-48b5a0ddb2ac5266828c7411c5e6ff82",
 "trylink": ""
 },
 {
 "type": "China HD",
-"livelink": "0",
+"livelink": "https://a0504dae9c72cdb02b00ddedf6df4973.livehwc3.cn/pull.juqihuo.com/live/stream-435066_lhd.m3u8?auth_key=1702492144-0-0-fc1480effef54ab71b885987216f0a78&sub_m3u8=true&user_session_id=af1a2f8aabc1b056fbbd4362d0050aff&edge_slice=true",
 "trylink": ""
 },
 {
 "type": "China SD",
-"livelink": "0",
+"livelink": "https://a0504dae9c72cdb02b00ddedf6df4973.livehwc3.cn/pull.juqihuo.com/live/stream-435066_lsd.m3u8?auth_key=1702492144-0-0-fc1480effef54ab71b885987216f0a78&sub_m3u8=true&user_session_id=af1a2f8aabc1b056fbbd4362d0050aff&edge_slice=true",
 "trylink": ""
 },
 {
