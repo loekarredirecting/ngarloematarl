ff6e29cda9b74f3fc826e3801207b9d71848e06f
htetrkar <95409875+loekarredirecting@users.noreply.github.com>
2023-12-15 15:16:02 +0630

    Update slider.php

--- slider.php
+++ slider.php
@@ -1,3 +1,7 @@
+
 [{ "title": "New Live", "link": "https://play.google.com/store/apps/details?id=com.pro.burmese_tv", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/avietnam.jpg" },
 { "title": "Advice", "link": "https://m.me/109810678671415", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/advice.jpg" }, 
-{ "title": "Advertising", "link": "https://play.google.com/store/apps/details?id=com.pro.burmese_tv", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/adslocal.jpg" }]
+{ "title": "Advertising", "link": "https://play.google.com/store/apps/details?id=com.pro.burmese_tv", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/adslocal.jpg" },
+{ "title": "Yes", "link": "Question are not start Yet..", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/quiz.jpg" }
+
+]
